9cac734c21ddb36eb03dc6f1e6ebf9c93bb3dd73
vyantik <akarizidu2@gmail.com>
2025-09-09 22:56:27 +0300

    feat(db, payments): add resource payments and few models in db(user_subscriptions, transactions)

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 
 import { AuthModule } from './auth/auth.module'
+import { PaymentsModule } from './payments/payments.module'
 import { PlansModule } from './plans/plans.module'
 import { UsersModule } from './users/users.module'
 
 @Module({
-	imports: [AuthModule, UsersModule, PlansModule],
+	imports: [AuthModule, UsersModule, PlansModule, PaymentsModule],
 })
 export class ApiModule {}
---
+++ src/api/payments/payments.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from '@nestjs/common'
+
+import { PaymentsService } from './payments.service'
+
+@Controller('payments')
+export class PaymentsController {
+	constructor(private readonly paymentsService: PaymentsService) {}
+}
